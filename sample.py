2a8a1299d5ed587db0cd9e4ce00117dac56c27d7
kprivafy <89973356+kprivafy@users.noreply.github.com>
2022-01-06 14:47:37 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,19 @@
+import requests
+import time
+import json
+import base64
+import random
+import datetime
+
+
+def add_user(headers,payload):
+        print("")
+        print("add user ")
+        url= "https://dashboard.lp.parsecs307.com/api/v1/accounts/pranav.srivastava+veea02@privafy.com/users"
+        print("url : "+url)
+
+        print(payload)
+        response = requests.request('POST', url, headers = headers, json = payload, verify=False,timeout=200)
+        time.sleep(5)
+        print(response.status_code)
+        print(response.text)
